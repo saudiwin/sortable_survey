d37e5b9d33803b82067a5bc96842b9d432526c55
Bob Kubinec <bobkubinec@gmail.com>
2025-01-03 14:38:39 -0500

    update to use rank_list_question

--- R/rank_list.R
+++ R/rank_list.R
@@ -18,6 +18,122 @@ as_label_tags <- function(labels) {
   )
 }
 
+#' Create a ranking item list.
+#'
+#' @description Creates a ranking item list using the `SortableJS` framework,
+#' and generates an `htmlwidgets` element.  The elements of this list can be
+#' dragged and dropped in any order.
+#'
+#' You can embed a ranking question inside a `learnr` tutorial, using
+#' [question_rank()].
+#'
+#' To embed a `rank_list` inside a shiny app, see the Details section.
+#'
+#' @details
+#'
+#' You can embed a `rank_list` inside a Shiny app, to capture the preferred
+#' ranking order of your user.
+#'
+#' The widget automatically updates a Shiny output, with the matching
+#' `input_id`.
+#'
+#'
+#' @param input_id output variable to read the plot/image from.
+#'
+#' @param labels A character vector with the text to display inside the widget.
+#'   This can also be a list of html tag elements.  The text content of each
+#'   label or label name will be used to set the shiny `input_id` value.
+#'   To create an empty `rank_list`, use `labels = list()`.
+#'
+#' @param text Text to appear at top of list.
+#'
+#' @param css_id This is the css id to use, and must be unique in your shiny
+#'   app. This defaults to the value of `input_id`, and will be appended to the
+#'   value "rank-list-container", to ensure the CSS id is unique for the
+#'   container as well as the labels.
+#'   If NULL, the function generates an id of the form
+#'   `rank_list_id_1`, and will automatically increment for every `rank_list`.
+#'
+#' @param class A css class applied to the rank list.  This can be used to
+#'   define custom styling.
+#'
+#' @param orientation Set this to "horizontal" to get horizontal orientation of
+#'   the items.
+#'
+#' @template options
+#'
+#' @seealso [update_rank_list], [sortable_js], [bucket_list] and [question_rank]
+#'
+#' @export
+#' @importFrom utils modifyList
+#' @importFrom htmltools  tagList tags
+#' @example inst/examples/example_rank_list.R
+#' @example inst/examples/example_rank_list_multidrag.R
+#' @example inst/examples/example_rank_list_swap.R
+#' @examples
+#' ## Example of a shiny app
+#' if (interactive()) {
+#'   app <- system.file("shiny/rank_list/app.R", package = "sortable")
+#'   shiny::runApp(app)
+#' }
+#'
+rank_list <- function(
+    text = "",
+    labels,
+    input_id,
+    css_id = input_id,
+    options = sortable_options(),
+    orientation = c("vertical", "horizontal"),
+    class = "default-sortable"
+) {
+  if (is.null(css_id)) {
+    css_id <- increment_rank_list()
+  }
+  orientation = match.arg(orientation)
+  if (orientation == "horizontal") class = paste0(class, " horizontal")
+  assert_that(is_sortable_options(options))
+  assert_that(is_input_id(input_id))
+
+  options$onSort <- chain_js_events( # nolint
+    options$onSort, # nolint
+    sortable_js_capture_input(input_id)
+  )
+  options$onLoad <- chain_js_events( # nolint
+    options$onLoad, # nolint
+    sortable_js_capture_input(input_id),
+    sortable_js_set_empty_class(css_id)
+  )
+
+  title_tag <- if (!is.null(text) && nchar(text) > 0) {
+    tags$p(class = "rank-list-title", text)
+  } else {
+    NULL
+  }
+
+  label_tags <- as_label_tags(labels)
+
+  rank_list_tags <- tagList(
+    tags$div(
+      class = paste("rank-list-container", paste(class, collapse = " ")),
+      id = as_rank_list_id(css_id),
+      title_tag,
+      tags$div(
+        class = "rank-list",
+        id = css_id,
+        label_tags
+      )
+    ),
+    sortable_js(
+      css_id = css_id,
+      options = options
+    ),
+    rank_list_dependencies()
+  )
+
+  as_rank_list(rank_list_tags)
+
+}
+
 
 #' Create a ranking item list.
 #'
@@ -195,4 +311,107 @@ update_bucket_list <- function(css_id, header = NULL,
   inputId <- paste0("bucket-list-", css_id)
   message <- dropNulls(list(id = inputId, header = header))
   session$sendInputMessage(inputId, message)
+}
+
+#' Ranking question for use in surveydown
+#'
+#' @description This question is a clone of [rank_list] but with
+#' some modifications to allow it to be used with in the [surveydown]
+#' R package. For more information about function options,
+#' see [rank_list] documentation.
+#'
+#'
+#'
+#' @param input_id output variable to read the plot/image from.
+#'
+#' @param labels A character vector with the text to display inside the widget.
+#'   This can also be a list of html tag elements.  The text content of each
+#'   label or label name will be used to set the shiny `input_id` value.
+#'   To create an empty `rank_list`, use `labels = list()`.
+#'
+#' @param text Text to appear at top of list.
+#'
+#' @param css_id This is the css id to use, and must be unique in your shiny
+#'   app. This defaults to the value of `input_id`, and will be appended to the
+#'   value "rank-list-container", to ensure the CSS id is unique for the
+#'   container as well as the labels.
+#'   If NULL, the function generates an id of the form
+#'   `rank_list_id_1`, and will automatically increment for every `rank_list`.
+#'
+#' @param class A css class applied to the rank list.  This can be used to
+#'   define custom styling.
+#'
+#' @param orientation Set this to "horizontal" to get horizontal orientation of
+#'   the items.
+#' @param output_width Width of the html widget. Need to set for
+#' compatibility with Quarto and surveydown.
+#'
+#' @param output_height Height of the html widget. Need to set for
+#' compatibility with Quarto and surveydown.
+#'
+#' @template options
+#'
+#'
+#' @export
+#' @importFrom utils modifyList
+#' @importFrom htmltools  tagList tags
+rank_list_survey <- function(
+    text = "",
+    labels,
+    input_id,
+    css_id = input_id,
+    options = sortable_options(),
+    orientation = c("vertical", "horizontal"),
+    class = "default-sortable",
+    output_width="100%",
+    output_height="100%"
+) {
+  if (is.null(css_id)) {
+    css_id <- increment_rank_list()
+  }
+  orientation = match.arg(orientation)
+  if (orientation == "horizontal") class = paste0(class, " horizontal")
+  assert_that(is_sortable_options(options))
+  assert_that(is_input_id(input_id))
+
+  options$onSort <- chain_js_events( # nolint
+    options$onSort, # nolint
+    sortable_js_capture_input(input_id)
+  )
+  options$onLoad <- chain_js_events( # nolint
+    options$onLoad, # nolint
+    sortable_js_capture_input(input_id),
+    sortable_js_set_empty_class(css_id)
+  )
+
+  title_tag <- if (!is.null(text) && nchar(text) > 0) {
+    tags$p(class = "rank-list-title", text)
+  } else {
+    NULL
+  }
+
+  label_tags <- as_label_tags(labels)
+
+  rank_list_tags <- tagList(
+    tags$div(
+      class = paste("rank-list-container", paste(class, collapse = " ")),
+      id = as_rank_list_id(css_id),
+      title_tag,
+      tags$div(
+        class = "rank-list",
+        id = css_id,
+        label_tags
+      )
+    ),
+    sortable_js(
+      css_id = css_id,
+      options = options,
+      width=output_width,
+      height=output_height
+    ),
+    rank_list_dependencies()
+  )
+
+  as_rank_list(rank_list_tags)
+
 }
